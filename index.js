3f443ec1a64305499951beb259820c3d9201023f
Angela Perez <angela.perez@flatironschool.com>
2018-08-24 01:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 function handleJSON(movie) {
-
+  
+  const titanicButton = document.getElementById('Titanic');
+  const terminatorButton = document.getElementById('Terminator');
+  
+  titanicButton.addEventListener('click', (event) => {
+    //do something
+  })
+  
+  terminatorButton.addEventListener('click', (event) => {
+    //do something
+  })
+  
+  function updateHTMLContent() {
+    let title = document.getElementById('title');
+    let director = document.getElementById('director');
+    let genre = document.getElementById('genre');
+    let filmRating = document.getElementById('FilmRating');
+    
+    title.innerHTML = currentMovie.title;
+    director.innerHTML = currentMovie.director
+    genre.innerHTML = currentMovie.genre
+    filmRating.innerHTML = currentMovie.filmRating;
+    poster.setAttribute('src', currentMovie.poster);
+    description.innerHTML = currentMovie.description;
+    audienceScore.innerHTML = currentMovie.audienceScore;
+    let castInnerHTML = "<ul></ul>"
+    
+    for (let i = 0; i < currentMovie.cast.length; i++) {
+     let role = currentMovie.cast[i].role;
+     let actor = currentMovie.cast[i].actor;
+     castInnerHTML += ("<li>" + role + " : " + actor + "</li>")
+      
+    }
+    castInnerHTML += "</ul>"
+    cast.innerHTML = castInnerHTML
+  }
+/*
     let json = movies[movie]
     document.getElementById('title').innerHTML = json.title
     document.getElementById('director').innerHTML = json.director
@@ -35,4 +71,5 @@ window.onload = () => {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', handleButton)
   }
+  */
 };
