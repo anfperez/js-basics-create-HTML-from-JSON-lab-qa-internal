01783b6b4477bfd0c4d210d0efb5b337c661c9e2
Angela Perez <angela.perez@flatironschool.com>
2018-08-24 01:03:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-function handleJSON(movie) {
-  
-  const titanicButton = document.getElementById('Titanic');
-  const terminatorButton = document.getElementById('Terminator');
-  
+document.addEventListener("DOMContentLoaded", (e) => {
+  // invoking a function here will make sure it happens on page load
+  // your code here
+  updateHTMLContent('Titanic')
+  const titanicButton = document.getElementById('Titanic')
+  const terminatorButton = document.getElementById('Terminator 2')
   titanicButton.addEventListener('click', (event) => {
-    //do something
+    //do something here
+    updateHTMLContent('Titanic')
   })
-  
   terminatorButton.addEventListener('click', (event) => {
-    //do something
+    // do something
+    updateHTMLContent('Terminator 2')
   })
+});
+
+function updateHTMLContent(movieName) {
+  let title = document.getElementById('title')
+  let director = document.getElementById('director')
+  let genre = document.getElementById('genre')
+  let filmRating = document.getElementById('filmRating')
+  let poster = document.getElementById('poster')
+  let description = document.getElementById('description')
+  let audienceScore = document.getElementById('audienceScore')
+  let currentMovie = movies[movieName]
+  let cast = document.getElementById('cast')
+  let reviews = document.getElementById('reviews')
   
-  function updateHTMLContent() {
-    let title = document.getElementById('title');
-    let director = document.getElementById('director');
-    let genre = document.getElementById('genre');
-    let filmRating = document.getElementById('FilmRating');
-    
-    title.innerHTML = currentMovie.title;
-    director.innerHTML = currentMovie.director
-    genre.innerHTML = currentMovie.genre
-    filmRating.innerHTML = currentMovie.filmRating;
-    poster.setAttribute('src', currentMovie.poster);
-    description.innerHTML = currentMovie.description;
-    audienceScore.innerHTML = currentMovie.audienceScore;
-    let castInnerHTML = "<ul></ul>"
-    
-    for (let i = 0; i < currentMovie.cast.length; i++) {
-     let role = currentMovie.cast[i].role;
-     let actor = currentMovie.cast[i].actor;
-     castInnerHTML += ("<li>" + role + " : " + actor + "</li>")
-      
-    }
-    castInnerHTML += "</ul>"
-    cast.innerHTML = castInnerHTML
+  title.innerHTML = currentMovie.title
+  director.innerHTML = currentMovie.director
+  genre.innerHTML = currentMovie.genre
+  filmRating.innerHTML = currentMovie.filmRating
+  poster.setAttribute('src', currentMovie.poster)
+  description.innerHTML = currentMovie.description
+  audienceScore.innerHTML = currentMovie.audienceScore
+  let castInnerHTML = ""
+  let reviewInnerHTML = ""
+  
+  for (let castMember of currentMovie.cast) {
+    let role = castMember.role
+    let actor = castMember.actor
+    castInnerHTML += ("<li>" + role + ": " + actor + "</li>")
   }
+  castInnerHTML += ""
+  cast.innerHTML = castInnerHTML
+  for (let review of currentMovie.reviews) {
+    let userName = review.username
+    let content = review.content
+    reviewInnerHTML += ("<p>" + userName + ": " + content + "</p>")
+  }
+  reviewInnerHTML += ""
+  reviews.innerHTML = reviewInnerHTML
+}
 /*
     let json = movies[movie]
     document.getElementById('title').innerHTML = json.title
